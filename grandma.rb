35f108d062f25044586c050db36ecde7c7220135
Erin Ponsonby <erinponsonby@gmail.com>
2019-03-15 15:28:12 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(says)
-  if says.downcase || says.capitalize
+  if says.downcase
     return "HUH?! SPEAK UP, SONNY!"
   elsif says == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
